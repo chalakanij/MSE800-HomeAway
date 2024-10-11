732fbc10fb2ba0e1c9b4af52836406a82cacb770
Medhaka Sanjeewa Vithanage <vithanagevms@gmail.com>
2024-10-11 15:03:31 +1300

    Read me updated, Employer code in employee creation added

--- Backend/app/services/user_service.py
+++ Backend/app/services/user_service.py
@@ -5,9 +5,10 @@ from sqlalchemy.orm import Session
 from app.db.models import User, UserRole
 from app.schemas.user import EmployerCreate, EmployeeCreate, AdminCreate
 from app.utils.hashing import hash_password, verify_password
+from fastapi import HTTPException
 
 
-def generate_company_code(company_name):
+def _generate_company_code(company_name):
     COMPANY_CODE_LENGTH = 8
     if re.search(r"\s", company_name):
         first, second = company_name.upper().rsplit(maxsplit=1)
@@ -24,20 +25,32 @@ def create_employer(db: Session, user: EmployerCreate):
     user_input = user.dict()
     del user_input['password']
     user_input['role'] = UserRole.EMPLOYER
-    user_input['employer_code'] = generate_company_code(user_input['company_name'])
+    user_input['employer_code'] = _generate_company_code(user_input['company_name'])
+    user_input['parent_user_id'] = 0
     db_user = User(**user_input, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+# check the employer code
+def _get_employer_by_code(db: Session, code):
+    user = db.query(User).filter(User.employer_code == code).first()
+    if user:
+        return user
+    return None
+
 def create_employee(db: Session, user: EmployeeCreate):
     hashed_password = hash_password(user.password)
     user_input = user.dict()
+    employer = _get_employer_by_code(db, user_input['employer_code'])
+    print (employer)
+    if employer is None:
+        raise HTTPException(status_code=404, detail="Invalid Employer Code!")
     del user_input['password']
     user_input['role'] = UserRole.EMPLOYEE
     user_input['company_name'] = ''
-    user_input['employer_code'] = ''
+    user_input['parent_user_id'] = employer.id
     db_user = User(**user_input, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
@@ -51,6 +64,7 @@ def create_admin(db: Session, user: AdminCreate):
     user_input['role'] = UserRole.ADMIN
     user_input['company_name'] = ''
     user_input['employer_code'] = ''
+    user_input['parent_user_id'] = 0
     db_user = User(**user_input, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
