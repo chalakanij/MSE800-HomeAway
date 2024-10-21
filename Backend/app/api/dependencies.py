2ec1f4f7250dd46fd66a29cbaf8e879b50fef832
VithanageVMS <vithanagevms@gmail.com>
2024-10-22 12:34:06 +1300

    UserRole validation on particular routes.

--- Backend/app/api/dependencies.py
+++ Backend/app/api/dependencies.py
@@ -1,6 +1,13 @@
+from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.exc import SQLAlchemyError, OperationalError
 from app.db import SessionLocal
+from typing import List
+from app.db.models import User as UserModel
+from fastapi.security import OAuth2PasswordBearer
+from app.auth.jwt import create_access_token, verify_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # Dependency to get the database session
 def get_db():
@@ -22,3 +29,26 @@ def get_db():
         )
     finally:
         db.close()
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+    user = db.query(UserModel).filter(UserModel.email == token_data.email).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+def role_required(required_roles: List[str]):
+    def role_checker(user: dict = Depends(get_current_user)):
+        if user.role not in required_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You do not have access to this resource"
+            )
+        return user
+    return role_checker
--- Backend/app/api/routes/dashboard_routes.py
+++ Backend/app/api/routes/dashboard_routes.py
@@ -1,28 +1,26 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Path
-from fastapi_pagination import Params, add_pagination
-from fastapi_pagination.iterables import LimitOffsetPage
+from fastapi import APIRouter
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordRequestForm
 from app.services.dashboard_service import DashboardService
-from app.db import SessionLocal
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from app.db.models import User
-from fastapi_pagination import Page
+from fastapi import Depends
+from app.db.models import User,UserRole
 from app.api.dependencies import get_db
-from app.api.routes.user_routes import get_current_user
-
+from app.api.dependencies import get_current_user
+from app.api.dependencies import role_required
 
 router = APIRouter()
 
 
-@router.get("/employee_dashboard")
+@router.get("/employee_dashboard", dependencies=[Depends(role_required(["EMPLOYEE"]))])
 def get_employee_dashboard(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     dashboard_service = DashboardService(db)
     return dashboard_service.get_employee_dashboard(current_user)
 
-@router.get("/employer_dashboard")
+@router.get("/employer_dashboard", dependencies=[Depends(role_required([UserRole.EMPLOYER]))])
 def get_employee_dashboard(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     dashboard_service = DashboardService(db)
     return dashboard_service.get_employer_dashboard(current_user)
 
+@router.get("/employer_dashboard", dependencies=[Depends(role_required([UserRole.ADMIN]))])
+def get_employee_dashboard(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    dashboard_service = DashboardService(db)
+    return dashboard_service.get_admin_dashboard(current_user)
