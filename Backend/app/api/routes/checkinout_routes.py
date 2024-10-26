86a382df12adb3558cc2dba5606879208e8e7fd8
VithanageVMS <vithanagevms@gmail.com>
2024-10-27 10:30:16 +1300

    Endpoint description added

--- Backend/app/api/routes/checkinout_routes.py
+++ Backend/app/api/routes/checkinout_routes.py
@@ -15,25 +15,28 @@ from app.api.dependencies import role_required
 router = APIRouter()
 
 @router.get("/checkin_status", response_model=Optional[CheckInOut],
-            dependencies=[Depends(role_required([UserRole.EMPLOYER, UserRole.EMPLOYEE]))])
+            dependencies=[Depends(role_required([UserRole.EMPLOYER, UserRole.EMPLOYEE]))],
+            description="Require: EMPLOYEE | EMPLOYER, To check current checkin status")
 def check_current_checkin_status(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     ch_service = CheckInOutService(db)
     return ch_service.get_current_checkin(current_user)
 
-@router.post("/checkin", response_model=CheckInOut, dependencies=[Depends(role_required([UserRole.EMPLOYER, UserRole.EMPLOYEE]))])
+@router.post("/checkin", response_model=CheckInOut, dependencies=[Depends(role_required([UserRole.EMPLOYER, UserRole.EMPLOYEE]))],
+             description="EMPLOYEE | EMPLOYER, perform checkin, out_time is optional to checkout")
 def create_checkin_record(checkin: CheckInRequest, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     ch_service = CheckInOutService(db)
     return ch_service.create_checkin(current_user, checkin)
 
 @router.get("/checkinout", response_model=Page[CheckInOut], dependencies=[Depends(role_required([UserRole.EMPLOYEE, UserRole.EMPLOYER]))],
-            description="Employee| Employer")
+            description="EMPLOYEE| EMPLOYER, Read checkinout records, if EMPLOYEE only their records, if EMPLOYER, all employees under their account")
 def get_checkinout_records( db: Session = Depends(get_db), current_user: User = Depends(get_current_user),
                page:int | None = 1, size:int |None = 10, user_id:int = None, project_id:int = None):
     param = Params(page=page, size=size)
     ch_service = CheckInOutService(db)
     return ch_service.get_checkinout(current_user, param, user_id, project_id)
 
-@router.put("/checkinout", response_model=Optional[CheckInOut], dependencies=[Depends(role_required([UserRole.EMPLOYER, UserRole.EMPLOYEE]))])
+@router.put("/checkinout", response_model=Optional[CheckInOut], dependencies=[Depends(role_required([UserRole.EMPLOYER, UserRole.EMPLOYEE]))],
+            description="EMPLOYEE | EMPLOYER, Update checkinout record")
 def update_checkinout_record(checkin_update: CheckinUpdateRequest, db: Session = Depends(get_db),
                            current_user: User = Depends(get_current_user)):
     ch_service = CheckInOutService(db)
