d1ac9b9b9cbef6e26719fd64b36896310bab6d8e
VithanageVMS <vithanagevms@gmail.com>
2024-10-27 09:45:18 +1300

    Add App title and other arguments

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from app.api import routes
 from app.db import init_db
 from fastapi.middleware.cors import CORSMiddleware
+from app.config import APP_NAME
 
-app = FastAPI()
+app = FastAPI(
+    title=APP_NAME,
+    description="Project based time tracking for employees as SaaS",
+    version="1.0.0",
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+    contact={
+        "name": "Support Team",
+        "email": "homeaway_support@yoobee-college.ac.nz",
+        "url": "https://www.homeaway_solutions.com/support",
+    },
+    terms_of_service="https://www.homeaway_solutions.com/terms",
+)
 origins = [ "http://localhost:4200" ], # Angular app
 
 
