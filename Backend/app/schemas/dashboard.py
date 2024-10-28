84423c66abea66ca04feee81e5437af9fbb7ce1f
VithanageVMS <vithanagevms@gmail.com>
2024-10-29 00:05:17 +1300

    fixup! dashboard response schema fixed

--- Backend/app/schemas/dashboard.py
+++ Backend/app/schemas/dashboard.py
@@ -2,7 +2,8 @@ from pydantic import BaseModel, EmailStr
 from typing import List, Optional
 from datetime import datetime
 
-from app.schemas.project import ProjectStatusCount
+from app.schemas.project import ProjectStatus, ProjectStatusCount
+
 
 class EmployeeDashboard(BaseModel):
     projects: Optional[List[ProjectStatusCount]]
@@ -12,14 +13,14 @@ class EmployeeDashboard(BaseModel):
 class ProjectHours(BaseModel):
     project_id: int
     project_title: str
-    project_status: str
+    project_status: ProjectStatus
     work_hours:int
-    calculated:Optional[int]=0
+    calculated:Optional[float]=0
 
 class EmployerDashboard(BaseModel):
     projects: Optional[List[ProjectStatusCount]]
     employees: Optional[List[ProjectStatusCount]] = None
-    project_hours: Optional[ProjectHours] = None
+    project_hours: Optional[List[ProjectHours]] = None
 
 class AdminDashboard(BaseModel):
     employees: Optional[List[ProjectStatusCount]]
