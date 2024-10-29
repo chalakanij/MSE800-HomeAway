c149703bd8d7e198544ffefa5ca47a1b1eef33c7
VithanageVMS <vithanagevms@gmail.com>
2024-10-29 16:12:17 +1300

    Routes make async

--- Backend/app/api/routes/user_routes.py
+++ Backend/app/api/routes/user_routes.py
@@ -16,19 +16,19 @@ from app.db.models import UserRole
 router = APIRouter()
 
 @router.post("/employer_register", response_model=EmployerOutput)
-def register(user: EmployerCreate, db: Session = Depends(get_db)):
+async def register(user: EmployerCreate, db: Session = Depends(get_db)):
     user_service = UserService(db)
     db_user = user_service.create_employer(user)
     return db_user
 
 @router.post("/employee_register", response_model=EmployeeOutput)
-def register(user: EmployeeCreate, db: Session = Depends(get_db)):
+async def register(user: EmployeeCreate, db: Session = Depends(get_db)):
     user_service = UserService(db)
     db_user = user_service.create_employee(user)
     return db_user
 
 @router.post("/login", response_model=Token)
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user_service = UserService(db)
     user = user_service.authenticate_user(form_data.username, form_data.password)
     if not user:
@@ -50,7 +50,7 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
 
 @router.get("/employees", response_model=Page[EmployeeOutput], dependencies=[Depends(role_required([UserRole.EMPLOYER]))],
             description="Required: EMPLOYER, get employees under an employer")
-def list_users(db: Session = Depends(get_db), current_user: User = Depends(get_current_user),
+async def list_users(db: Session = Depends(get_db), current_user: User = Depends(get_current_user),
                page:int | None = 1, size:int |None = 10):
     param = Params(page=page, size=size)
     user_service = UserService(db)
@@ -58,7 +58,7 @@ def list_users(db: Session = Depends(get_db), current_user: User = Depends(get_c
 
 @router.get("/employers", response_model=Page[EmployerOutput], dependencies=[Depends(role_required([UserRole.ADMIN]))],
             description="Required: ADMIN, get all employers")
-def list_users(db: Session = Depends(get_db), current_user: User = Depends(get_current_user),
+async def list_users(db: Session = Depends(get_db), current_user: User = Depends(get_current_user),
                page:int | None = 1, size:int |None = 10):
     param = Params(page=page, size=size)
     user_service = UserService(db)
@@ -66,15 +66,15 @@ def list_users(db: Session = Depends(get_db), current_user: User = Depends(get_c
 
 @router.delete("/employees", dependencies=[Depends(role_required([UserRole.EMPLOYER, UserRole.ADMIN]))],
                description="Required: EMPLOYER | ADMIN, Deactivate list of employees")
-def deactivate_users_list(users: UserDeactivateRequest, db:Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+async def deactivate_users_list(users: UserDeactivateRequest, db:Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     user_service = UserService(db)
     return user_service.deactivate_users(users)
 
 @router.get("/profile",  response_model=EmployeeOutput, description="Get current user's profile")
-def read_profile(db: Session = Depends(get_db), current_user: EmployeeOutput = Depends(get_current_user)):
+async def read_profile(db: Session = Depends(get_db), current_user: EmployeeOutput = Depends(get_current_user)):
     return current_user
 
 @router.put("/profile", response_model=EmployeeOutput, description="Update current user's profile")
-def update_profile_request(user_profile: ProfileInput ,db: Session = Depends(get_db), current_user: EmployeeOutput = Depends(get_current_user)):
+async def update_profile_request(user_profile: ProfileInput ,db: Session = Depends(get_db), current_user: EmployeeOutput = Depends(get_current_user)):
     user_service = UserService(db)
     return user_service.update_profile(current_user, user_profile)
